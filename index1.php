331727a7b0f8d29cb3023320d483dce416b46b5c
arpit191001 <60779810+arpit191001@users.noreply.github.com>
2020-03-28 20:23:34 +0530

    initial commit

---
+++ index1.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <title>EDA.FI</title>
+  <!-- Bootstrap styles -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet"/>
+    <!-- Customize styles -->
+      
+      <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
+<link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet">
+	
+	
+      <link href="style.css"rel="stylesheet"/>
+    <!-- font awesome styles -->
+	<link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet">
+	
+  </head>
+<body>
+
+<!--
+Lower Header Section 
+-->
+<div class="container">
+<div id="gototop"> </div>
+<header id="header">
+    
+<div class="row">
+	<div class="span4">
+	<h1>
+        <a class="logo" href="index.html"><span></span> 
+		<img src="logo1.png"alt="eda logo">
+	</a>
+	</h1>
+	</div>
+	<div class="span4">
+	<div class="offerNoteWrapper">
+	<h1 class="dotmark">
+	
+	</h1>
+	</div>
+	</div>
+	<div class="span4 alignR">
+	<p><br> <strong> Support (24/7) :  0800 3432  </strong><br><br></p>
+	<span class="btn btn-mini">[ 2 ] <span class="icon-shopping-cart\"></span></span>
+	<span class="btn btn-warning btn-mini">$</span>
+	<span class="btn btn-mini">&pound;</span>
+	<span class="btn btn-mini">&euro;</span>
+	</div>
+</div>
+</header>
+
+
+<div class="navbar">
+	  <div class="navbar-inner">
+		<div class="container">
+		  <a data-target=".nav-collapse" data-toggle="collapse" class="btn btn-navbar">
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		  </a>
+		  <div class="nav-collapse">
+			<ul class="nav">
+			  <li class="active"><a href="retrieving.php">Home	</a></li>
+			  <li class=""><a href="retrieving.php">Sale</a></li>
+			  <li class=""><a href="grid-view.html">Help</a></li>
+			  <li class=""><a href="three-col.html">Recommendations</a></li>
+			  <li class=""><a href="four-col.html">Gift Card</a></li>
+			</ul>
+              <form action="#" class="navbar-search pull-left">
+			  <input type="text" placeholder="Search here" class="search-query span2">
+                  <img src="https://img.icons8.com/material/24/000000/search--v1.png" alt="materials">
+			</form>
+              <ul class="nav pull-right">
+                     
+			<li class="dropdown">
+				<a data-toggle="dropdown" class="dropdown-toggle" href="#">Language <b class="caret"></b></a>
+                
+                  <div class="dropdown-menu">
+				<form class="form-horizontal languageFrm">
+				  <div class="control-group">
+					
+                      <p><img src="flag1.png" alt="English">ENGLISH [UK]</p>
+                      <p><img src="flag2.png" alt="swedish">SWEDISH</p>
+                      <p><img src="flag3.png" alt="finish"> FINNISH /SUOMI</p>
+                      <p><img src="flag4.png">LUXEMBOURGISH</p>
+                      
+                    </div>
+                      </form>
+                </div>
+                </li>
+            
+                  
+			<li class="dropdown">
+				<a data-toggle="dropdown" class="dropdown-toggle" href="#"><span class="icon-lock"></span> Login <b class="caret"></b></a>
+				<div class="dropdown-menu">
+				<form class="form-horizontal loginFrm">
+				  <div class="control-group">
+					<input type="text" class="span2" id="inputEmail" placeholder="Email">
+				  </div>
+				  <div class="control-group">
+					<input type="password" class="span2" id="inputPassword" placeholder="Password">
+				  </div>
+				  <div class="control-group">
+					<label class="checkbox">
+					<input type="checkbox"> Remember me
+					</label>
+					<button type="submit" class="shopBtn btn-block">Sign in</button>
+				  </div>
+				</form>
+				</div>
+			</li>
+			</ul>
+		  </div>
+		</div>
+	  </div>
+	</div>
+	
